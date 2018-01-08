bc220206860251458d02b0c9117cdfd9c54f0cda
Jessica <mis.zhaoyu@gmail.com>
2018-01-08 10:53:50 +0800

    R

--- R/restaurant.R
+++ R/restaurant.R
@@ -1,11 +1,10 @@
 restaurant = function(Id,OpenDate,City,P1,P2,P6,P17,P18,P20,P21,P23,P25,P28,P32,P36){
 
-  input$OpenDate=as.Date(input$OpenDate)
-  input$year=as.integer(format(input$OpenDate, "%Y"))
-  input$YearsSince1900 = as.numeric(input$year - 1900)
+  OpenDate=as.Date(OpenDate)
+  year=as.integer(format(OpenDate, "%Y"))
+  YearsSince1900 = as.numeric(year - 1900)
 
-  input=data.frame(Id,OpenDate,City,CityGroup,Type,P1,P2,P3,P4,P5,P6,P7,P8,P9,P10,P11,P12,P13,P14,P15,P16,P17,P18,P19,P20,
-                   P21,P22,P23,P24,P25,P26,P27,P28,P29,P30,P31,P32,P33,P34,P35,P36,P37,YearsSince1900)
+  input=data.frame(Id,OpenDate,City,P1,P2,P6,P17,P18,P20,P21,P23,P25,P28,P32,P36,YearsSince1900)
 
   int_data=as.character(c("P1","P6","P17","P18","P20","P21","P23","P25","P32","P36"))
   num_data=as.character(c("P2","P28","YearsSince1900"))
