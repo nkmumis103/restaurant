be4412028b3915ffc8e3e1df16928c23394a2bc8
Jessica <mis.zhaoyu@gmail.com>
2018-01-08 11:13:33 +0800

    R

--- R/restaurant.R
+++ R/restaurant.R
@@ -4,7 +4,7 @@ restaurant = function(Id,OpenDate,City,P1,P2,P6,P17,P18,P20,P21,P23,P25,P28,P32,
   year=as.integer(format(OpenDate, "%Y"))
   YearsSince1900 = as.numeric(year - 1900)
 
-  input=data.frame(P1,P2,P6,P17,P18,P20,P21,P23,P25,P28,P32,P36,YearsSince1900)
+  #input=data.frame(P1,P2,P6,P17,P18,P20,P21,P23,P25,P28,P32,P36,YearsSince1900)
 
   int_data=as.character(c("P1","P6","P17","P18","P20","P21","P23","P25","P32","P36"))
   num_data=as.character(c("P2","P28","YearsSince1900"))
@@ -20,7 +20,7 @@ restaurant = function(Id,OpenDate,City,P1,P2,P6,P17,P18,P20,P21,P23,P25,P28,P32,
 
   important_input = c(P1,P2,P6,P17,P18,P20,P21,P23,P25,P28,P32,P36,YearsSince1900)
 
-  newdata = 1#exp(predict(res_model, important_input))
+  newdata = important_input#exp(predict(res_model, important_input))
 
 
   list(
